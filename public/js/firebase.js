a92bb87b1c9b9b2d60a9dce967851c369b4894ba
Arthur <arthur.felipe1993@gmail.com>
2025-09-08 13:46:40 -0300

    Correção dos parâmetros do firebase

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,25 +1,22 @@
-// Importa as funções necessárias dos SDKs do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// TODO: Substitua o seguinte pelas credenciais de configuração do seu projeto Firebase
-// Encontre-as em: Configurações do projeto > Geral > Seus apps > App da Web
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCiiWyMqDmF17aQBA-fwNo5ByotEsA7fn0",
   authDomain: "olomi-7816a.firebaseapp.com",
+  databaseURL: "https://olomi-7816a-default-rtdb.firebaseio.com",
   projectId: "olomi-7816a",
   storageBucket: "olomi-7816a.firebasestorage.app",
   messagingSenderId: "562685499782",
-  appId: "1:562685499782:web:28732864ca37c610c43407",
-  measurementId: "G-WREW35G7PM"
+  appId: "1:562685499782:web:23616d2db4738093c43407",
+  measurementId: "G-2FSC9P97MX"
 };
 
-// Inicializa o Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exporta os serviços do Firebase que serão utilizados noutras partes do site
-export const auth = getAuth(app);
-export const db = getFirestore(app, 'olomi');
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
